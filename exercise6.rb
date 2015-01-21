cbe98723ac0ab14e4998528a87f4f1327f4c2cf9
lizhodgson <lizhodgson@gmail.com>
2015-01-21 16:29:29 -0500

    created a class to add an item to my grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,26 @@
-grocery_list = ["rice", "anchovies", "oranges", "apples", "figs"]
-grocery_list.each { |i| puts "* #{i}" }
 
+class GroceryList
+
+	def initialize(initial_list)
+		@list = initial_list
+	end
+
+	def print
+		@list.each { |i| puts "* #{i}" }
+	end
+
+	def add(item)
+		@list << item
+	end
+
+end
+
+list = GroceryList.new ["rice", "anchovies", "oranges", "apples", "figs"]
+list.print
+puts "\n"
+
+puts "Adding some ground round"
+list.add "ground round"
+
+list.print
 
