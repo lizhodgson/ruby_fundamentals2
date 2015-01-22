18c05878ea6e8417112a24e7e9afd12064360531
lizhodgson <lizhodgson@gmail.com>
2015-01-22 09:18:43 -0500

    Using interpolation and defining methods on hashes.

--- exercise7.rb
+++ exercise7.rb
@@ -1,5 +1,14 @@
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
-  :cohort3 => 22
+  :cohort3 => 22,
 }
+
+def list_cohorts(hash)
+	hash.each do |x,y|
+		puts " #{x} : #{y}"
+	end
+	
+end
+
+list_cohorts(students)
