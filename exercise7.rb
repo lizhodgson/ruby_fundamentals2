43f98e6e0369a5e6d2fe3fa4a230e98bc5f47a4a
lizhodgson <lizhodgson@gmail.com>
2015-01-22 09:39:06 -0500

    Hash exercises complete.

--- exercise7.rb
+++ exercise7.rb
@@ -12,3 +12,24 @@ def list_cohorts(hash)
 end
 
 list_cohorts(students)
+
+# Add 43 as the amount of students for cohort 4.
+# Output all of the cohort names with the keys method.
+# The classrooms have been expanded: increase each cohort number by 5% and display the new results.
+# Delete the 2nd cohort, and redisplay the cohorts
+
+students[:cohort4] = 43
+
+list_cohorts(students)
+
+puts students.keys
+
+students.each { |x,y| students[x] = (y * 1.05).round}
+
+list_cohorts(students)
+
+students.delete(:cohort2)
+
+list_cohorts(students)
+
+
